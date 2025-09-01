d3d45cc8b88b291bf9b9a6e8517c57c665415dae
Dan Federman <dfed@me.com>
2025-08-31 21:29:28 -0700

    fix export

--- CLI/prepare-coverage-reports.sh
+++ CLI/prepare-coverage-reports.sh
@@ -2,17 +2,14 @@
 set -e
 
 function exportlcov() {
-    build_type=$1
-    executable_name=$2
+    executable_name=$1
 
-    executable=$(find "${directory}" -type f -name $executable_name)
-    profile=$(find "${directory}" -type f -name 'Coverage.profdata')
+    executable=$(find .build/*/*/$executable_name.xctest/Contents/*/$executable_name -type f)
+    profile=$(find .build -type f -name 'default.profdata')
     output_file_name="$executable_name.lcov"
 
     can_proceed=true
-    if [[ $build_type == watchOS* ]]; then
-        echo "\tAborting creation of $output_file_name – watchOS not supported."
-    elif [[ -z $profile ]]; then
+    if [[ -z $profile ]]; then
         echo "\tAborting creation of $output_file_name – no profile found."
     elif [[ -z $executable ]]; then
         echo "\tAborting creation of $output_file_name – no executable found."
@@ -22,13 +19,8 @@ function exportlcov() {
 
         output_file="$output_dir/$output_file_name"
         echo "\tExporting $output_file"
-        xcrun llvm-cov export -format="lcov" $executable -instr-profile $profile > $output_file
+        xcrun llvm-cov export -format="lcov" $executable -instr-profile $profile >$output_file
     fi
 }
 
-for directory in $(git rev-parse --show-toplevel)/.build/derivedData/*/; do
-    build_type=$(basename $directory)
-    echo "Finding coverage information for $build_type"
-
-    exportlcov $build_type 'SwiftPackageGeneratorTests'
-done
+exportlcov 'SwiftPackageGeneratorTests'
